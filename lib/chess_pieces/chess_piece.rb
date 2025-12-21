8c2e9b14539c5223e015edb65a61113acacecc8f
Kai Hong <ricedev10@gmail.com>
2025-12-21 14:05:03 -0500

    able to add positions

--- lib/chess_pieces/chess_piece.rb
+++ lib/chess_pieces/chess_piece.rb
@@ -8,6 +8,10 @@ class ChessPiece
     @black = black
   end
 
+  def add_positions(position1, position2)
+    [position1[0] + position2[0], position1[1] + position2[1]]
+  end
+
   def to_s
     @color == :white ? @white : @black
   end
