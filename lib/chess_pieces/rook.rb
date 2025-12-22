6b33677db1c50ec42fe438450595c3f2d94bacc4
Kai Hong <ricedev10@gmail.com>
2025-12-22 12:52:13 -0500

    remove duplicate function

--- lib/chess_pieces/rook.rb
+++ lib/chess_pieces/rook.rb
@@ -11,29 +11,6 @@ class Rook < ChessPiece
 
   def can_move_to?(board, new_position)
     current_position = @position
-    unless current_position[0] == new_position[0] || current_position[1] == new_position[1]
-      raise RangeError,
-            "Must be perpendicular #{current_position} -> #{new_position}"
-    end
-
-    position = current_position
-    offset = [
-      current_position[0] == new_position[0] ? 0 : 1,
-      current_position[1] == new_position[1] ? 0 : 1
-    ]
-
-    loop do
-      position = add_positions(position, offset)
-
-      return true if position == new_position
-
-      object = board[position[0]][position[1]]
-      raise StandardError, "Blocked by #{object} at #{position}" unless object.nil?
-    end
-  end
-
-  def can_move_to?(board, new_position)
-    current_position = @position
     if current_position[0] != new_position[0] && current_position[1] != new_position[1]
       raise RangeError,
             'Must be perpendicular'
